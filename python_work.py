bf501743d43a98a2eda19c002a78ab3d1c560901
Garrett Wendorff <124766112+Gwendorff1207@users.noreply.github.com>
2025-01-13 01:26:56 -0600

    Add files via upload

---
+++ python_work.py
@@ -0,0 +1,11 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+first_name = "ada"
+last_name = "lovelace"
+
+print(f"{first_name} {last_name}")
+
