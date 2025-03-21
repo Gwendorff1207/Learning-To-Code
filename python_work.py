55a7a92c538cf6b3ccdff780c1816ec5d459e07e
Garrett Wendorff <garrettwendorff@gmail.com>
2025-03-20 21:07:41 -0500

    Update python_work.py

--- python_work.py
+++ python_work.py
@@ -1,11 +1,2 @@
 message = "Hello Python world!"
 print(message)
-
-message = "Hello Python Crash Course world!"
-print(message)
-
-first_name = "ada"
-last_name = "lovelace"
-
-print(f"{first_name} {last_name}")
-
